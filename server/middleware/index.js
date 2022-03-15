75d63cdf3892c3573a8efc4ccb08775443569d22
mo7amedehab97 <gazacityface@gmail.com>
2022-03-15 21:37:55 +0200

    create the routes for the signin routes

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-undef */
 const express = require('express')
 const compression = require('compression')
 const cookieParse = require('cookie-parser')
 const { join } = require('path')
+const { router } = require('../router/route');
 
 module.exports = (app) => {
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
-  app.use(express.static(join(__dirname, '..', '..', 'client')))
   app.use(compression())
   app.disabled('x-powered-by')
   app.use(cookieParse())
-}
+  app.use(express.static(join(__dirname, '..', '..', 'client')))
+  app.use(router);
+
+};
