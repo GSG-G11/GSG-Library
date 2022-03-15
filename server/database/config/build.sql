ae073c319203260608b8aa2fd2b18f34c3800976
karam-zomlut <zomlutk@gmail.com>
2022-03-15 14:26:51 +0200

    Finish Database Setup Relates #3

---
+++ server/database/config/build.sql
@@ -0,0 +1,58 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, books, favourits CASCADE;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    isAdmin BOOLEAN NOT NULL
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    author VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE favourit_books (
+    id SERIAL PRIMARY KEY,
+    user_id INT FOREIGN KEY,
+    book_id INT FOREIGN KEY
+);
+
+INSERT INTO
+    books (name, description, category, image, author)
+    values
+    (
+        'The Diary of a Young Girl',
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+        'History',
+        'shorturl.at/mHSZ5',
+        'Anne Frank'
+    ),
+    (
+        'Night (The Night Trilogy, #1)',
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+        'Classics',
+        'shorturl.at/ovDQ9',
+        'Elie Wiesel'
+    ),
+    (
+        'To Kill a Mockingbird',
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+        'Arts',
+        'shorturl.at/imAF7',
+        'Harper Lee'
+    ),
+    (
+        'The Great Gatsby',
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit',
+        'Classics',
+        'shorturl.at/rsSX9',
+        'F.Scott Fitzgerald'
+    );
