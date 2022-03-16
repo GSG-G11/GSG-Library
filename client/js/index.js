2eac1d5c04d778c760d05a7883b6274088be966f
mo7amedehab97 <gazacityface@gmail.com>
2022-03-16 15:31:08 +0200

    create validation for the signin in frontend

---
+++ client/js/index.js
@@ -0,0 +1,53 @@
+const submitBtn = document.getElementById('btn')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+submitBtn.addEventListener('click', () => {
+  checkInputs()
+})
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const emailValue = email.value.trim()
+  const passwordValue = password.value.trim()
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank')
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email')
+  } else {
+    setSuccessFor(email)
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'password cannot be blank')
+  } else if (!isPassword(passwordValue)) {
+    setErrorFor(password, 'Not a valid password')
+  } else {
+    setSuccessFor(password)
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement
+  const small = formControl.querySelector('small')
+  formControl.className = 'form-control error'
+  small.innerText = message
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement
+  formControl.className = 'form-control success'
+}
+const isEmail = (email) => {
+  const emailLength = email.split('').length
+  return emailLength > 10 && email.split('').includes('@')
+}
+const isPassword = (password) => {
+  const passwordLength = password.split('').length
+  return (
+    passwordLength > 8 &&
+    (password.split('').includes('!') ||
+      password.split('').includes('$') ||
+      password.split('').includes('*'))
+  )
+}
