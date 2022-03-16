3b35b1e27a66bb88840bf332c7dd6fe8ed84525b
mo7amedehab97 <gazacityface@gmail.com>
2022-03-16 17:11:39 +0200

    send data to the backend

--- client/js/index.js
+++ client/js/index.js
@@ -3,27 +3,32 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 submitBtn.addEventListener('click', () => {
   checkInputs()
-})
 
+
+})
 function checkInputs() {
-  // trim to remove the whitespaces
   const emailValue = email.value.trim()
   const passwordValue = password.value.trim()
 
   if (emailValue === '') {
     setErrorFor(email, 'Email cannot be blank')
+    return false
   } else if (!isEmail(emailValue)) {
     setErrorFor(email, 'Not a valid email')
+    return false
   } else {
     setSuccessFor(email)
   }
 
   if (passwordValue === '') {
     setErrorFor(password, 'password cannot be blank')
+    return false
   } else if (!isPassword(passwordValue)) {
     setErrorFor(password, 'Not a valid password')
+    return false
   } else {
     setSuccessFor(password)
+    signIn()
   }
 }
 
@@ -44,10 +49,19 @@ const isEmail = (email) => {
 }
 const isPassword = (password) => {
   const passwordLength = password.split('').length
-  return (
-    passwordLength > 8 &&
-    (password.split('').includes('!') ||
-      password.split('').includes('$') ||
-      password.split('').includes('*'))
-  )
+  return passwordLength > 8
+}
+
+const signIn = () => {
+  const request = {
+    method: 'post',
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }
+  return fetch('/signin', request)
+    .then((result) => result.json())
+    .then((data) => console.log(data))
 }
